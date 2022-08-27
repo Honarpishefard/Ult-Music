d02f894d83598606249b004af38f212b9ee545e8
Honarpishefard <108079784+Honarpishefard@users.noreply.github.com>
2022-08-28 02:48:36 +0430

    Add files via upload

--- js/modules/musicSourceHandler.js
+++ js/modules/musicSourceHandler.js
@@ -6,7 +6,7 @@ const musicContainer = document.querySelector(".music-container");
 export default function musicSourceHandler() {
     musicSource().forEach((song) => {
         const element = `
-        <a href="./../html/album-info.html" id="${song.id}" target="_blank" class="col-2 song-sec p-0 d-flex flex-column align-items-center music-div text-black">
+        <a href="./../html/album-info-page.html" id="${song.id}" target="_blank" class="col-2 song-sec p-0 d-flex flex-column align-items-center music-div text-black">
         <img class="song-cover" src="${song.cover}" alt="${song.name} ${song.artist}">
         <p class="fw-semibold fs-5 my-1">${song.name}</p>
         <p class="fw-normal fs-6 mb-0">${song.artist}</p>
