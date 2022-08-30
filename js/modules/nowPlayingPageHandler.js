88bc81a64ab599d457a1492cc769ff3a40065765
Honarpishefard <108079784+Honarpishefard@users.noreply.github.com>
2022-08-30 22:24:39 +0430

    Add files via upload

--- js/modules/nowPlayingPageHandler.js
+++ js/modules/nowPlayingPageHandler.js
@@ -5,12 +5,15 @@ import shuffleMusicFn from "./../utils/shuffle.js";
 
 
 const nextBttn = document.getElementById("nextBttn");
+const audio = document.getElementById("audio");
 const musicProgressInput = document.getElementById("musicProgressInput");
 const previousBttn = document.getElementById("previousBttn");
 const shuffleBttn = document.getElementById("shuffleBttn");
 const replayBttn = document.getElementById("replayBttn");
 const replayBttnPath = document.querySelectorAll(".replay-bttn-path");
-
+const musicVolume = document.getElementById("musicVolume");
+const musicVolumeBar = document.getElementById("musicVolumeBar");
+const musicVolumeIcon = document.getElementById("musicVolumeIcon");
 
 
 export default function nowPlayingPageHandler() {
@@ -66,4 +69,29 @@ export default function nowPlayingPageHandler() {
             replayBttnPath[1].style.fill = "#d2d2d2";
         }
     });
+
+    musicVolume.addEventListener("change", () => {
+        audio.volume = musicVolume.value / 10;
+        musicVolumeBar.style.width = (musicVolume.value) + "rem";
+    });
+
+    let volumeMute = false;
+    musicVolumeIcon.addEventListener("click", () => {
+        volumeMute = !volumeMute;
+
+        function muteToggleHandler() {
+            if (volumeMute) {
+                audio.volume = 0;
+                musicVolumeBar.style.width = "0rem";
+                musicVolumeIcon.src = "./../img/now playing svg/volume-xmark-solid.svg";
+            } else {
+                audio.volume = musicVolume.value / 10;
+                musicVolumeBar.style.width = (musicVolume.value) + "rem";
+                musicVolumeIcon.src = "./../img/now playing svg/volume-high-solid.svg";
+            }
+        };
+
+        muteToggleHandler();
+    });
+    
 };
